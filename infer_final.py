2e586b4af9f5d0480cdfa5fe231e1ea6973523af
Jobstone <jobstone2048@126.com>
2023-06-13 12:02:48 +0800

    test

--- infer_final.py
+++ infer_final.py
@@ -29,8 +29,7 @@ def signal_handler(signal, frame):
 
 parser = HfArgumentParser(ModelArguments)
 model_args, = parser.parse_args_into_dataclasses(args=["--checkpoint_dir",
-                                                       "/kaggle/working/model/ChatGLM-Efficient-Tuning/checkpoint4"
-                                                       "/checkpoint-5000"])
+                                                       "./ChatGLM-Efficient-Tuning/checkpoint4/checkpoint-5000"])
 model, tokenizer = load_pretrained(model_args)
 model = model.cuda()
 model.eval()
